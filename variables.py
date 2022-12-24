9fc2e18d31924e64607b9334212694876072da4c
Johnny Pham <johnpham7496@gmail.com>
2022-12-24 11:49:48 -0700

    adding invoice caluculator

---
+++ variables.py
@@ -0,0 +1,44 @@
+school = 'Minneapolis College'
+print(school)
+
+name = 'Johnny'
+print(name)
+
+print(name + ' goes to ' + school)
+
+favorite_food = 'pizza'
+print(favorite_food)
+
+print('My name is ' + name + ', I got to ' + school + ', and my favorite food is ' + favorite_food)
+
+credits_taken= '14'
+print(credits_taken)
+
+trained_astronauts = 563
+space_tourists = 7
+total_people_in_space = trained_astronauts + space_tourists
+print(total_people_in_space)
+
+miles_to_potland = 1425.81
+miles_to_helena = 907.29
+
+miles_to_helena_to_potland = miles_to_potland - miles_to_helena
+print(miles_to_helena_to_potland)
+
+popcorn = 4.25
+bags_purchased = 3
+# something about popcorn
+popcorn_cost = popcorn*bags_purchased
+print(popcorn_cost)
+# hurry up so I can be an engineer
+"""" so this is block comments wow imma write a whole sotry for some of these guys to read and firgure out because lmao they suck lowkey kinda cool justin and his girl always attached is not"""
+
+print('This semester I am taking ' + str(credits_taken) + ' credits')
+print('It is ' + str(miles_to_helena_to_potland) + ' miles Helena to Portland')
+
+name = input('Please enter your name: ')
+print('Hi ' + name)
+
+miles_to_seattle = float(input('How many miles to Seattle? '))
+km_seattle = miles_to_seattle * 1.609
+print('It is ' + str(km_seattle) + ' kilometers to Seattle')
