6047f83987329b9e87d9760612d04a08cbb06ba1
Johnny Pham <johnpham7496@gmail.com>
2022-12-25 20:04:01 -0700

    added quiz score letter grace and notes to fulltime.py

--- week3/full_time_student.py
+++ week3/full_time_student.py
@@ -8,3 +8,19 @@ elif number_of_credits >= 6:
     print('You are part-time student')
 else:
     print('You are less than part-time student')
+
+# grading quiz score and giving a grade
+quiz_score = float(input('Please enter the quiz score, out of 100: '))
+
+# if else statement to determine grace level based on quiz score
+if quiz_score >= 90:
+    print('Your letter grade is A')
+
+elif quiz_score >= 80:
+    print('Your letter grade is B')
+
+elif quiz_score >= 70:
+    print('Your letter grade is C')
+
+else:
+    print('You failed the test')
