f6a84a81d42cc083c37194546401cd0021c413d1
Johnny Pham <johnpham7496@gmail.com>
2022-12-27 12:28:51 -0700

    changes str value in while loop

--- week4/roll_dice.py
+++ week4/roll_dice.py
@@ -4,6 +4,6 @@ want_to_quit = ''     # empty strings are considered false
 
 while not want_to_quit:
     dice_value = random.randint(1, 6)
-    print('You rolled a ' + str(dice_value))
+    print(f'You rolled a {dice_value}')
     # not typing anything and pressing enter will set want_to_quit to an empty string
     want_to_quit = input('Press enter to roll again, any other key to quit')
