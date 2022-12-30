513f9ba3f3ddc84de1b6774aed9c1baadfec1f51
Johnny Pham <johnpham7496@gmail.com>
2022-12-29 20:04:48 -0700

    added function to change letters to lowercase

---
+++ week5/language_library_functinos.py
@@ -0,0 +1,14 @@
+# you're telling me there's a faster way to find the unicode smh I could've just used the upper() function
+# this code is more efficient
+example_class_cde = 'itec 1234'
+# uppercase function to change the letters from the original string
+uppercase_class_code = example_class_cde.upper()
+print(f'The class code in uppercase is {uppercase_class_code}')
+
+print()
+# string
+email = 'CONTACT@MINNEAPOLIS.EDU'
+# function calling the uppercase letters and changeing them to lowercase
+lowercase_email = email.lower()
+# output
+print(f'The lowercase of the email is {lowercase_email}')
