8b37a8d42f3c2eea8eeb179a7d9bee828f34952c
Johnny Pham <johnpham7496@gmail.com>
2022-12-30 23:32:08 -0700

    added notes to class_codes

--- week5/class_codes.py
+++ week5/class_codes.py
@@ -1,13 +1,19 @@
 def codes(class_codes):
+    # if else determining whether the class codes is in between the numbers
     if class_codes < 2000 or class_codes <= 1000:
+        # return function is user is a first year
         return print('You are a first year')
     else:
+        # return function if user is a second year
         return print('You are a second year')
 
 
 def main():
+    # variable
     class_codes = int(input('Enter your four-digit class code: '))
+    # calling to codes function
     grade = codes(class_codes)
+    # output after calling function
     print(grade)
 
 
