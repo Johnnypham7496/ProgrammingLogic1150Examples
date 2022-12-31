ae1a98c0d6c4f0b53b57e7a917647f30660417f7
Johnny Pham <johnpham7496@gmail.com>
2022-12-30 17:49:35 -0700

    added more examples to calling_functions file

--- week5/calling_functions.py
+++ week5/calling_functions.py
@@ -11,7 +11,29 @@ def main():
 
 main()
 
+print()
 
+def main():
+    print(f'The cubes of the numbers 1 through 10 are: ')
+    for number in range(1, 11):
+        print_cube(number)
+def print_cube(x):
+    number_cubed = x**3
+    print(f'The cube of {x} is {number_cubed}')
+
+main()
+
+print()
+def main():
+    string = input('Please enter a string: ')
+    repeat = int(input('How many times do you want this string to repeat: '))
+    result = string_repeater(string, repeat)
+    print(result)
+def string_repeater(text, n):
+    repeat_string = text * n
+    return repeat_string
+
+main()
 
 print()
 def rectangle_area(height, width):
