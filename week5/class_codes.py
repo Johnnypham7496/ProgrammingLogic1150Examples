6a97db424bc829f7912bb8b5212d7bd2df9962f9
Johnny Pham <johnpham7496@gmail.com>
2023-01-01 22:09:27 -0700

    finally got class_codes to validate a 4 digit number and added squares_from_words file

--- week5/class_codes.py
+++ week5/class_codes.py
@@ -1,23 +1,30 @@
-def codes(class_codes):
-    # if else determining whether the class codes is in between the numbers
-    # trying to create a statement where it makes the user enter in at least 4 digits numbers: unsuccessful
-    while not class_codes != 4:
-        if 1000 <= class_codes < 2000:
-            # return function is user is a first year
-            return print('You are a first year')
-        else:
-            # return function if user is a second year
-            return print('You are a second year')
+def first_or_second_year(class_codes):
+    if 1000 <= class_codes < 2000:
+        # return function is user is a first year
+        return print('You are a first year')
     else:
-        return print('Class codes are 4 digit numbers')
+        # return function if user is a second year
+        return print('You are a second year')
+
+
+def codes_valid(class_codes):
+    # if statement to determine
+    if class_codes < 1000 or class_codes > 3000:
+        return False
+    else:
+        return True
 
 
 def main():
     # variable
     class_codes = int(input('Enter your four-digit class code: '))
-    # calling to codes function
-    codes(class_codes)
-    # original: output after calling function print()
+    # calling to codes_valid function
+    while not codes_valid(class_codes):
+        print('Class codes are 4 digit numbers')
+        # prompts the user to reenter the class code if the return is false
+        class_codes = int(input('Enter your four-digit class code, should be a 4 digit number: '))
+        # calls to the first_or_second_year function
+    first_or_second_year(class_codes)
     # no need for output in main because the function itself has an output with the return of if else statements
 
 
---
+++ week5/squares_from_words.py
@@ -0,0 +1,23 @@
+def main():
+
+    while user_wants_to_continue('make squares from words'):
+        word = input('Please enter the word to print in a square: ')
+        word_square(word)
+
+    print('Thanks for using the program!')
+
+
+def word_square(word):
+    for letter in range(len(word)):
+        print(word)
+
+
+
+def user_wants_to_continue(task):
+    response = input(f'Do you want to {task}? Enter "Y" for yes, anything else for no. ')
+    if response.upper() == 'Y':
+        return True
+    return False
+
+
+main()
