91ebc5d7c2a6ba6bee5b218bfe76874394ff3db4
Johnny Pham <johnpham7496@gmail.com>
2023-01-03 15:38:08 -0700

    created lists example for week6

---
+++ week6/lists.py
@@ -0,0 +1,19 @@
+# a list of strings
+villians = ['Voldemort', 'Darth Vader', 'Sauron']
+
+# a list of in numbers
+quize_score = [10,9,10,8]
+
+# a list of float numbers
+rainfall = [0.8, 1.1, 1.3, 2.1, 0.6, 0.2, 7.6]
+
+# a mixture of data types
+mixture = ['Hello', 100, 5.55] # not very useful tho
+
+# a lists of lists
+lists_of_lists = [['a', 'b', 'c']], [['x', 'y', 'z']]
+
+
+my_classes = ['Programming Logic', 'Info Tech Skills', 'Data Communications']
+
+my_first_class = my_classes[0]
