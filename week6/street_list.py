9d895508e7d78637b5df017eab52364c318b42a4
Johnny Pham <johnpham7496@gmail.com>
2023-01-03 20:16:39 -0700

    added first list example from scratch

---
+++ week6/street_list.py
@@ -0,0 +1,12 @@
+streets = ['Lake', 'Hennepin', 'Lyndale']
+first_street = streets[0]
+third_street = streets[2]
+print(first_street)
+print(third_street)
+
+streets[1] = 'Portland'
+
+print(streets)
+
+for street in streets:
+    print(street)
