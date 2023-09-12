9f0ba83b9f3c22ba4df942e98d30343910b3fedd
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-12 18:48:58 -0500

    added print statements to see where the setup is and teardown

--- mock_test/test_employee.py
+++ mock_test/test_employee.py
@@ -5,15 +5,17 @@ from employee import Employee
 class TestEmployee(unittest.TestCase):
 
     def setUp(self):
+        print('setUp')
         self.emp_1 = Employee('Johnny', 'Pham', 70000)
         self.emp_2 = Employee('Anh', 'Huynh', 70000)
 
 
     def tearDown(self):
-        pass
+        print('tearDown\n')
 
 
     def test_email(self):
+        print('test_email')
         self.assertEqual(self.emp_1.email, 'Johnny.Pham@email.com')
         self.assertEqual(self.emp_2.email, 'Anh.Huynh@email.com')
 
@@ -25,6 +27,7 @@ class TestEmployee(unittest.TestCase):
 
 
     def test_fullname(self):
+        print('test fullname')
         self.assertEqual(self.emp_1.fullname, 'Johnny Pham')
         self.assertEqual(self.emp_2.fullname, 'Anh Huynh')
 
@@ -36,6 +39,7 @@ class TestEmployee(unittest.TestCase):
 
 
     def test_apply_raise(self):
+        print('test apply_raises')
         self.emp_1.apply_raises()
         self.emp_2.apply_raises()
 
