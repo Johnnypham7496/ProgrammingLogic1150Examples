b032aae3ae0007a543d43c574d2d8749183bba44
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-12 18:44:53 -0500

    created reuseable variables for test cases with setUp method

--- mock_test/test_employee.py
+++ mock_test/test_employee.py
@@ -4,44 +4,43 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
 
+    def setUp(self):
+        self.emp_1 = Employee('Johnny', 'Pham', 70000)
+        self.emp_2 = Employee('Anh', 'Huynh', 70000)
+
+
+    def tearDown(self):
+        pass
+
+
     def test_email(self):
-        emp_1 = Employee('Johnny', 'Pham', 70000)
-        emp_2 = Employee('Anh', 'Huynh', 70000)
+        self.assertEqual(self.emp_1.email, 'Johnny.Pham@email.com')
+        self.assertEqual(self.emp_2.email, 'Anh.Huynh@email.com')
 
+        self.emp_1.first = 'Anh'
+        self.emp_2.first = 'Johnny'
 
-        self.assertEqual(emp_1.email, 'Johnny.Pham@email.com')
-        self.assertEqual(emp_2.email, 'Anh.Huynh@email.com')
-
-        emp_1.first = 'Anh'
-        emp_2.first = 'Johnny'
-
-        self.assertEqual(emp_1.email, 'Anh.Pham@email.com')
-        self.assertEqual(emp_2.email, 'Johnny.Huynh@email.com')
+        self.assertEqual(self.emp_1.email, 'Anh.Pham@email.com')
+        self.assertEqual(self.emp_2.email, 'Johnny.Huynh@email.com')
 
 
     def test_fullname(self):
-        emp_1 = Employee('Johnny', 'Pham', 70000)
-        emp_2 = Employee('Anh', 'Huynh', 70000)
+        self.assertEqual(self.emp_1.fullname, 'Johnny Pham')
+        self.assertEqual(self.emp_2.fullname, 'Anh Huynh')
 
-        self.assertEqual(emp_1.fullname, 'Johnny Pham')
-        self.assertEqual(emp_2.fullname, 'Anh Huynh')
+        self.emp_1.first = 'Anh'
+        self.emp_2.first = 'Johnny'
 
-        emp_1.first = 'Anh'
-        emp_2.first = 'Johnny'
-
-        self.assertEqual(emp_1.fullname, 'Anh Pham')
-        self.assertEqual(emp_2.fullname, 'Johnny Huynh')
+        self.assertEqual(self.emp_1.fullname, 'Anh Pham')
+        self.assertEqual(self.emp_2.fullname, 'Johnny Huynh')
 
 
     def test_apply_raise(self):
-        emp_1 = Employee('Johnny', 'Pham', 70000)
-        emp_2 = Employee('Anh', 'Huynh', 70000)
+        self.emp_1.apply_raises()
+        self.emp_2.apply_raises()
 
-        emp_1.apply_raises()
-        emp_2.apply_raises()
-
-        self.assertEqual(emp_1.pay, 73500)
-        self.assertEqual(emp_2.pay, 73500)
+        self.assertEqual(self.emp_1.pay, 73500)
+        self.assertEqual(self.emp_2.pay, 73500)
 
 
 
