397fc35667bcc713cfcf4bb8175a5f134a713409
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-12 18:14:03 -0500

    created an example class of Employee to conduct mock testing

---
+++ mock_test/employee.py
@@ -0,0 +1,21 @@
+class Employee():
+
+    raise_amt = 1.05
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    
+    @property
+    def email(self):
+        return f'{self.first}{self.last}@email.com'
+    
+
+    def fullname(self):
+        return f'{self.first} {self.last}'
+    
+
+    def apply_raises(self):
+        self.pay= int(self.pay * self.raise_amt)
