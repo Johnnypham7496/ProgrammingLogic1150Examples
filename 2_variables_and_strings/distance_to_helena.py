affd9d362c8fe5eec26e8e7c20c6e43fbc09869d
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-13 15:21:54 -0500

    subtraction of one point from another

---
+++ 2_variables_and_strings/distance_to_helena.py
@@ -0,0 +1,11 @@
+""" Subtraction - calculating the difference to an intermediate point """
+
+
+miles_to_portland = 1425.81
+miles_to_helena = 907.29
+
+miles_helena_to_portland = miles_to_portland - miles_to_helena
+print(miles_helena_to_portland)
+
+
+print(f'It is {miles_helena_to_portland} miles from Helena to Portland.')
