86060b910b875714d39297bd1c182082fb3fa771
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-13 18:22:48 -0500

    added input for name and returned length of the name

---
+++ 2_variables_and_strings/length_of_string.py
@@ -0,0 +1,8 @@
+""" Combining imput and length - how long is the user's name? """
+
+
+name = input('Please enter your name: ')
+print(f'Hello, {name}')
+
+name_length = len(name)
+print(f'Your name is {name_length} letters long')
