638943b7a927ebb1587c0582bd2e7e097f78aa85
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-13 15:15:49 -0500

    added examples of variable types

---
+++ 2_variables_and_strings/declaring_variable.py
@@ -0,0 +1,33 @@
+""" Examples for creating string, int, and float variables """
+
+
+# Example string variables for storing test data
+username = 'Beyonce'
+creadit_card_type = 'Visa' 
+textbook_title = 'Automate the Boring Stuff'
+textbook_author = 'Al Sweigart'
+
+car = 'Toyota'
+
+player_name = 'Megaman'
+
+item_description = 'New Iphone 15, $1000'
+lastest_message = 'Do you want to get coffee?'
+
+
+# Examples of int (integer) variables, for storing whole numbers
+powerups_in_level = 4
+students_in_class = 100
+score = 100
+score_difference = -5
+initial_score = 0 
+
+
+
+#  Example float variables, for storing numbers with decimal places
+song_length = 187.21
+textbook_price = 100
+summer_temperature = 91.4
+winter_temperature = 20.9
+distance_to_portland = 1425.81
+headwind = 63.3
