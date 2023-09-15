6d383d3f9f190379ed8c0d927b4ea0f3c76b2310
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-15 17:18:10 -0500

    if statement to determine if it is a fulltime student or not

---
+++ 3_if_statements/fulltime_student.py
@@ -0,0 +1,6 @@
+""" Deciding if a student is a full time student.
+To be a full-time student, take 12 or more credits in a semster."""
+
+number_of_credits = int(input('Please enter a number of credits you are taking this semester: '))
+if number_of_credits == 12:
+    print('You are a full-time student')
