c4c5ba28b2a4de0a81064d37adf7eb746f21344b
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-15 16:08:56 -0500

    using len() to determine the length of users name

---
+++ 3_if_statements/count_letters_in_name.py
@@ -0,0 +1,8 @@
+"""
+Use the len() function to count letters in a name
+"""
+
+
+name = input('Please enter your name: ')
+letters_in_name = len(name)
+print(f'You have {letters_in_name} letters in your name')
