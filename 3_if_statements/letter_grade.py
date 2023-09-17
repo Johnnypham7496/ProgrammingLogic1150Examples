18808b98f1099cf71befad771972530330631f8a
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-17 16:22:19 -0500

    example of if, elif, and else statements to determine users grade

---
+++ 3_if_statements/letter_grade.py
@@ -0,0 +1,16 @@
+""" Using if, elif, elif else to convert a percent grade to a letter grade. """
+
+quiz_score = float(input('Please enter the quiz score, out of 100: '))
+
+# Convert quiz score to a letter grade
+
+if quiz_score >= 90:
+    print('Your letter grade is an A')
+elif quiz_score >= 80:
+    print('Your letter grade is a B')
+elif quiz_score >= 70:
+    print('Your letter grade is a C')
+elif quiz_score >= 60:
+    print('Your letter grade is a D')
+else:
+    print('Sorry, you have failed the quiz')
