c199ebd90bd0a01dcdbaa2a83022f43f3f19c2a2
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-17 19:15:29 -0500

    example of input password not being equal to variable

---
+++ 3_if_statements/password_not_equal.py
@@ -0,0 +1,13 @@
+"""
+Checking if the user enters the wrong password
+Checking if a value is not equal to another value using !=
+"""
+
+secret_password = 'kittens'   # Super top secret password
+
+password = input('Enter the secret password: ')
+
+if password != secret_password:
+    print('ACCESS DENIED!!!')
+else:
+    print('Welcome, authorized user')
