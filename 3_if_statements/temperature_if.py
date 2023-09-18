41e5725c2cc3d279d8436852d60ddb6608626bec
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-18 13:59:57 -0500

    temperature example using if else

---
+++ 3_if_statements/temperature_if.py
@@ -0,0 +1,10 @@
+"""
+A program with an if-elif-else statement to decide if the current temperature
+is above freezing, below freezing, or exactly freezing
+"""
+
+
+current_temperature = float(input('Enter the current temperature, in F: '))
+
+if current_temperature < 3:
+    print('It is below freezing')
