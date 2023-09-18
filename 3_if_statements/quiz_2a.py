3781530dbf50ce5254e9d3c0e370d840536a5a86
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-17 19:22:45 -0500

    example of quiz using not equals symbol

---
+++ 3_if_statements/quiz_2a.py
@@ -0,0 +1,15 @@
+"""
+Quiz program, version 2. This prints a message if the user gets the
+answer right or wrong.
+
+
+"""
+
+print('Quiz program!')
+
+answer = input('What is the capital of Wisconsin? ')  # It's Madison
+
+if answer != 'Madison':
+    print('Sorry, the answer is Madison.')
+else:
+    print('Correct!')
