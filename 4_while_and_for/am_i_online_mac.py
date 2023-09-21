848d651f38d9a1bcd9ee99f9da48caf7ea55bfbd
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-21 17:52:06 -0500

    git added notification sounds depending if device is mac or windows

---
+++ 4_while_and_for/am_i_online_mac.py
@@ -0,0 +1,24 @@
+from urllib import request
+from time import sleep
+import os
+
+# A URL for a website that we expect to be available, if we are online
+# Mac users, this may fail. Try http://www.google.com without the s
+url = 'https://www.google.com'
+
+seconds_to_sleep_between_checks = 3
+
+while True:
+    print('Checking if you are online...')
+    try:
+        # Open the URL. This will error/fail if you are not online.
+        request.urlopen(url).read()
+        print('You seem to be online')
+        # Shout out to the user! This only works on Macs
+        os.system('say hey user, you are online')
+    except:
+        print('You are NOT online')
+
+    print(f'Sleeping for {seconds_to_sleep_between_checks} seconds...')
+    print()
+    sleep(seconds_to_sleep_between_checks)
